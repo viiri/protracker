bba58d56f6429688f9d3e3d498c74d4e23a3c8c2
eightbitbubsy <eightbitbubsy@4421b666-268f-4d9d-8f69-d2436a73919e>
2018-02-03 14:16:22 +0000

    * Bundled SDL2 libs/includes for MinGW and edited the .bat. Now you only need to install MinGW and nothing more!

--- make-mingw.bat
+++ make-mingw.bat
@@ -10,7 +10,7 @@ del release\protracker.exe 2>NUL
 windres src\protracker.rc src\resource.o
 
 echo Compiling, please wait...
-mingw32-gcc src\*.c src\gfx\*.c src/resource.o -lmingw32 -lSDL2main -mwindows -lSDL2 -lm -Wall -Wno-unused-result -Wshadow -Winit-self -Wextra -Wunused -Wunreachable-code -Wredundant-decls -Wswitch-default -march=native -mtune=native -O3 -s -o release\protracker.exe
+mingw32-gcc src\*.c src\gfx\*.c src/resource.o -Imingw_sdl_include\include -Lmingw_sdl_include\lib -m32 -lmingw32 -lSDL2main -mwindows -lSDL2 -lm -Wall -Wno-unused-result -Wshadow -Winit-self -Wextra -Wunused -Wunreachable-code -Wredundant-decls -Wswitch-default -march=native -mtune=native -O3 -s -o release\protracker.exe
 del src\*.o src\gfx\*.o 2>NUL
 
 echo Done! The binary (protracker.exe) is in the folder named 'release'.
