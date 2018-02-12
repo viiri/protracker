8c2e31c63922d1731d1e2a615bcd7a0cceb2a9ad
eightbitbubsy <eightbitbubsy@4421b666-268f-4d9d-8f69-d2436a73919e>
2018-02-12 12:21:49 +0000

    * Removed -ldl from make-linux.sh (no idea why it was there?) * Fix fullscreen toggle going bananas on GNU/Linux (stupid bug in SDL2 port) * Fix compilation warnings on GNU/Linux

--- make-linux.sh
+++ make-linux.sh
@@ -3,7 +3,7 @@
 rm release/protracker &> /dev/null
 
 echo Compiling, please wait...
-gcc src/*.c src/gfx/*.c -lSDL2 -lm -ldl -Wall -Wno-unused-result -Wc++-compat -Wshadow -Winit-self -Wextra -Wunused -Wunreachable-code -Wredundant-decls -Wswitch-default -march=native -mtune=native -O3 -o release/protracker
+gcc src/*.c src/gfx/*.c -lSDL2 -lm -Wall -Wno-unused-result -Wc++-compat -Wshadow -Winit-self -Wextra -Wunused -Wunreachable-code -Wredundant-decls -Wswitch-default -march=native -mtune=native -O3 -o release/protracker
 
 rm src/*.o src/gfx/*.o &> /dev/null
 
